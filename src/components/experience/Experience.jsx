a6f82dd7ef061a48952a14e3907eb01126f99517
Samuel Ingvarsson <samuel.ingvarsson@hotmail.com>
2023-03-01 11:13:49 +0100

    add experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 import "./experience.css";
+import { AiFillHtml5 } from "react-icons/ai";
+import { DiCss3 } from "react-icons/di";
+import { BsFillBootstrapFill } from "react-icons/bs";
+import { DiJavascript1 } from "react-icons/di";
+import { FaAngular } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
 
 function Experience() {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What skills I have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <AiFillHtml5 className="experience__details-icon" />
+              <h4>HTML</h4>
+            </article>
+            <article className="experience__details">
+              <DiCss3 className="experience__details-icon" />
+              <h4>CSS</h4>
+            </article>
+            <article className="experience__details">
+              <BsFillBootstrapFill className="experience__details-icon" />
+              <h4>Bootstrap</h4>
+            </article>
+            <article className="experience__details">
+              <DiJavascript1 className="experience__details-icon" />
+              <h4>JavaScript</h4>
+            </article>
+            <article className="experience__details">
+              <FaAngular className="experience__details-icon" />
+              <h4>Angular</h4>
+            </article>
+            <article className="experience__details">
+              <FaReact className="experience__details-icon" />
+              <h4>React</h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Experience;
