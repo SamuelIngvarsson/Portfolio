d5c513a0035b82e68cdfc025192da71ffe4550ef
Samuel Ingvarsson <samuel.ingvarsson@hotmail.com>
2023-03-01 07:56:04 +0100

    add header

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CV from "../../assets/CV-Samuel-Ingvarsson.pdf";
+import Letter from "../../assets/Rekommendationsbrev.pdf";
+
+function CTA() {
+  return (
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href={Letter} download className="btn">
+        Download Reference
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's talk
+      </a>
+    </div>
+  );
+}
+export default CTA;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import Me from "../../assets/0E3E2BE4-CAB1-4678-8BDA-3A92060C0A4C_1_105_c.jpeg";
+import HeaderSocial from "./HeaderSocials";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Samuel Ingvarsson</h1>
+        <h5 className="text-light">Frontend/Web developer student</h5>
+        <CTA />
+        <HeaderSocial />
+        <div className="me">
+          <img src={Me} alt="" />
+        </div>
+      </div>
+    </header>
+  );
 }
 export default Header;
